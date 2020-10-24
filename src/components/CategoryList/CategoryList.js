2f0a6282e2c9a037c601e207ca186259b19c3f07
qwanysh <olimpsdu@gmail.com>
2020-10-24 16:45:51 +0600

    Add sticky category list

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -6,21 +6,23 @@ import {categories} from '../../consts';
 const CategoryList = () => {
   return (
       <Pane borderRight>
-        <Menu>
-          <Menu.Group>
-            <Menu.Item is={Link} to="/">All</Menu.Item>
-          </Menu.Group>
-          <Menu.Divider/>
-          <Menu.Group>
-            {categories.map(category => (
-                <Menu.Item
-                    key={category.id}
-                    is={Link}
-                    to={`/quotes/${category.id}`}
-                >{category.name}</Menu.Item>
-            ))}
-          </Menu.Group>
-        </Menu>
+        <Pane position='sticky' top={0}>
+          <Menu>
+            <Menu.Group>
+              <Menu.Item is={Link} to="/">All</Menu.Item>
+            </Menu.Group>
+            <Menu.Divider/>
+            <Menu.Group>
+              {categories.map(category => (
+                  <Menu.Item
+                      key={category.id}
+                      is={Link}
+                      to={`/quotes/${category.id}`}
+                  >{category.name}</Menu.Item>
+              ))}
+            </Menu.Group>
+          </Menu>
+        </Pane>
       </Pane>
   );
 };
