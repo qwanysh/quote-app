9c2645cba89585fe9fbb01f45c7971e1d264cce5
qwanysh <olimpsdu@gmail.com>
2020-10-24 12:06:30 +0600

    #1 Добавлен navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Heading, Pane} from 'evergreen-ui';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+      <Pane paddingY={20} display="flex" justifyContent="space-between" alignItems="center" borderBottom>
+        <Heading>Quotes App</Heading>
+        <Pane display="flex" alignItems="center">
+          <Pane is={Link} to="/" color="inherit" textDecoration="none" marginRight={20}>Quotes</Pane>
+          <Pane is={Link} to="/add-quote" color="inherit" textDecoration="none">Submit new Quote</Pane>
+        </Pane>
+      </Pane>
+  );
+};
+
+export default Navbar;
