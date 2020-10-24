b831cf974232e7570bc78a0c9ea36760fc48bd1a
qwanysh <olimpsdu@gmail.com>
2020-10-24 13:16:09 +0600

    #3 Добавлен фильтр по категорям в функцию getQuotes

--- src/helpers.js
+++ src/helpers.js
@@ -5,6 +5,15 @@ export const createQuote = quote => {
   return axios.post('/quotes.json', quote);
 };
 
+export const getQuotes = category => {
+  const url = category ? `/quotes.json?orderBy="category"&equalTo="${category.id}"` : '/quotes.json';
+  return axios.get(url).then(response => {
+    const quotes = response.data;
+    return quotes ? Object.entries(quotes)
+        .map(([id, quote]) => ({...quote, id})) : [];
+  });
+};
+
 export const isValid = quote => {
   const {author, text} = quote;
   return author.trim().length && text.trim().length;
